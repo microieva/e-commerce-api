046dce4c835569a3135b64fa52dfcd5e8f502b26
Ieva Vyliaudaite <ievavyliaudaite@Integrifys-MBP.lan>
2023-12-17 14:57:02 +0200

    implement getOrderItems endpoint

--- services/ordersService.ts
+++ services/ordersService.ts
@@ -1,6 +1,8 @@
 import mongoose from "mongoose"
 import OrdersRepo from "../models/Order"
-import { Order } from "../types/order";
+import ItemsRepo from "../models/Item"
+import ProductsRepo from "../models/Product"
+import { Order } from "../types/order"
 
 async function getAllOrders() {
   const orders = await OrdersRepo.find().exec();
@@ -17,6 +19,19 @@ async function getOrderById(orderId: string) {
     const id = new mongoose.Types.ObjectId(orderId);
     const order = await OrdersRepo.findOne({ _id: id}); 
     return order;
+}
+
+async function getOrderItems(orderId: string) {
+    const id = new mongoose.Types.ObjectId(orderId);
+    const items = await ItemsRepo.find({ orderId });
+
+    const orderItems = await Promise.all(
+            items.map(async (item) => {
+                const product = await ProductsRepo.findById(item.productId).populate("category");;
+                return { ...product?.toObject(), quantity: item.quantity };
+            })
+    );
+    return orderItems;
 }
 
 async function createOrder(userId: string, totalPrice: number) {
@@ -53,6 +68,7 @@ export default {
     getAllOrders,
     getOrdersByUserId,
     getOrderById,
+    getOrderItems,
     createOrder,
     updateOrder,
     deleteOrder,
